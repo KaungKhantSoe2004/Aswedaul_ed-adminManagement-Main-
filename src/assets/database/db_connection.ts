13d43a14dbc63ca1ff6af3a2a7f8483cf8eb10d7
KaungKhantSoe2004 <134781030+KaungKhantSoe2004@users.noreply.github.com>
2026-01-16 12:05:41 +0630

    socketUrlChanged

--- src/assets/database/db_connection.ts
+++ src/assets/database/db_connection.ts
@@ -2,7 +2,7 @@ import mysql, { Pool } from "mysql2/promise";
 
 export const db: Pool = mysql.createPool({
   host: "localhost",
-  user: "root",
-  password: "",
-  database: "aswedaul_ed_admin",
+  user: "zlllzpsz_aswedaul_ed_a",
+  password: "y5#3Wq_ho_U(AV{2",
+  database: "zlllzpsz_aswedaul_ed_a",
 });
